a628287df19158ed7a406420a69778873150e630
Kit Jonathan Te <kjonathante@gmail.com>
2018-09-27 17:14:35 -0700

    create webscrape module

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ var path = require('path')
 
 var express = require('express')
 var mongojs = require('mongojs')
+
+var webscrape = require('./src/utils/webscrape')
+
 var dbUrl = 'mongoapp_db'
 var collections = ['news']
 
@@ -16,6 +19,12 @@ var app = express()
 
 // Set up a static folder (public) for our web app
 app.use(express.static(path.join(__dirname, './src/public')))
+
+app.get('/test', async function(req, res) {
+  var stories = await webscrape()
+
+  res.json(stories)
+})
 
 app.get('/news', function(req, res) {
   db.news.find(
---
+++ src/utils/webscrape.js
@@ -0,0 +1,29 @@
+var request = require('request')
+var cheerio = require('cheerio')
+
+// A promise that returns an array of objects
+function webscrape() {
+  return new Promise(function(resolve,reject) {
+    request('https://www.nytimes.com/section/us', function(error, response, html) {
+      if (error) {
+        console.log(error)
+        return reject(error)
+      }
+
+      var $ = cheerio.load(html)
+      var stories = []       
+      $('#latest-panel [id^=story-id-]').each( function(index, element){
+        stories.push( {
+          storyid: $(element).attr('id'),
+          headline: $(element).find('article > div > a > div.story-meta > h2').text().trim(),
+          summary: $(element).find('article > div > a > div.story-meta > p').text().trim(),
+          url: $(element).find('article > div > a').attr('href'),
+          image: $(element).find('article > div > a > div.wide-thumb > img').attr('src')
+        } )
+      })
+      return resolve(stories)
+    })
+  })
+}
+
+module.exports = webscrape
