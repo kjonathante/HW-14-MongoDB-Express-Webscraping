30d7be9c32cc989da7c4da85442421e242a5537c
Kit Jonathan Te <kjonathante@gmail.com>
2018-09-27 21:06:27 -0700

    add get /story/:id endpoint and post /comment/:id endpoint

--- server.js
+++ server.js
@@ -21,13 +21,23 @@ var app = express()
 // Set up a static folder (public) for our web app
 app.use(express.static(path.join(__dirname, './src/public')))
 
+app.use(express.urlencoded({extended: true}))
+
 app.get('/news', async function(req, res) {
   var stories = await webscrape()
   storyModel.saveAll(db, stories)
   res.json(await storyModel.getAll(db))
 })
 
+app.get('/story/:id', async function(req, res) {
+ res.json(await storyModel.get(db, req.params.id))
+})
 
+app.post('/comment/:id', async function(req, res) {
+  // console.log(req.body.comment, req.params.id)
+  res.json(await storyModel.saveComment(db, req.params.id, req.body.comment))
+})
+ 
 app.listen(3000, 'localhost', function(){
   console.log('Listening on port localhost:3000')
 })
